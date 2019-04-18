1f56ef1945c27f85aed659963ad254bb82bf5cfc
lzh <13466083337@163.com>
2019-04-18 17:11:40 +0800

    finish router

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// import router
+var posts = require('./routes/posts');
+var post = require('./routes/post');
+var theme = require('./routes/theme')
+var topList = require('./routes/topList');
+var createAccount = require('./routes/createAccount');
+var token = require('./routes/token');
+var mineCollect = require('./routes/mineCollect');
+var minePost = require('./routes/minePost');
+var imageRouter = require('./routes/image');
+var commit = require('./routes/commit');
+var deleteRouter = require('./routes/delete');
+var collect = require('./routes/collect');
+var like = require('./routes/like');
+var review = require('./routes/review');
+var searchRouter = require('./routes/search');
 
 var app = express();
 
@@ -19,8 +35,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 设置跨域访问
+/*
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");//*表示允许的域名地址，本地则为'http://localhost' 
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Cache-Control", "no-store");
+  next();
+});
+*/
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', posts);
+app.use('/post', post);
+app.use('/theme', theme);
+app.use('/topList', topList);
+app.use('/createAccount', createAccount);
+app.use('/token', token);
+app.use('/mineCollect', mineCollect);
+app.use('/minePost', minePost);
+app.use('/image', imageRouter);
+app.use('/commit', commit);
+app.use('/delete', deleteRouter);
+app.use('/collect', collect);
+app.use('/like', like);
+app.use('/review', review);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
