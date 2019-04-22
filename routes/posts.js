e4357c036e688eccde70ec26f737c2ba8a9079dc
lzh <13466083337@163.com>
2019-04-22 17:19:52 +0800

    fix bug

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,55 @@ connection.connect();
 
 // 构造sql语句
 var sql = 'SELECT * FROM posts ORDER BY lastModified desc';
+var getUserSql = 'SELECT * FROM user_info WHERE id=?'
+
+// 数据获取方法（Promise）
+// ==获取帖子列表
+function getPosts(res, page){
+  return new Promise(function(resolve, reject){
+    connection.query(sql, function(err, result){
+      if(err){
+        base.sendErr(res, 2, err);
+        reject()
+        return;
+      }
+      var list = result.slice((page - 1) * 10, (page * 10));
+      if(list.length){
+        resolve(list);
+      } else{
+        base.sendErr(res, 3);
+        reject();
+      }
+    })
+  })
+}
+
+// ==获取用户信息
+function getUserInfo(res, postInfo){
+  return new Promise(function(resolve, reject){
+    connection.query(getUserSql, [postInfo.userId], function(err, result){
+      if(err){
+        console.log(err);
+        reject();
+      } else{
+        if(result.length){
+          var data = {
+            id: postInfo.id,
+            userImg: result[0].avatarUrl,
+            userName: result[0].nickName,
+            title: postInfo.title,
+            message: postInfo.message,
+            lastModified: postInfo.lastModified
+          };
+          resolve(data);
+        } else{
+          console.log('用户信息无效')
+          reject();
+        }
+      }
+    })
+  })
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,26 +71,22 @@ router.get('/', function(req, res, next) {
   // 参数错误返回结果
   if(params.page < 1 || !params.page){
     base.sendErr(res, 1);
-    return;
+  } else{
+    getPosts(res, params.page).then(function(result){
+      var list = [];
+      for(let i = 0; i < result.length; i++){
+        list.push(getUserInfo(res, result[i]));
+      }
+      Promise.all(list).then(function(resu){
+        res.send({
+          code: 0,
+          data: {
+            list: resu
+          }
+        })
+      })
+    })
   }
-  // 正常数据库数据提取和返回流程
-  connection.query(sql, function(err, result){
-    if(err){
-      base.sendErr(res, 2, err);
-      return;
-    }
-    var list = result.slice((params.page - 1) * 10, (params.page * 10));
-    if(list.length){
-      res.send({
-        code: 0,
-        data: {
-          list: list
-        }
-      });
-    } else{
-      base.sendErr(res, 3);
-    }
-  })
 });
 
 module.exports = router;
