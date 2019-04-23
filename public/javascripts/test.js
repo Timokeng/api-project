15f0aa16e756617f3edc79374544c788c08ce0b9
lzh <13466083337@163.com>
2019-04-23 16:52:36 +0800

    finish commit

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,10 +2,21 @@ var app = new Vue({
     el: "#app",
     data() {
         return {
-
+            post: {
+                title: '',
+                message: '',
+                images: null
+            }
         }
     },
     methods: {
-        
+        commitPost: function(){
+            let url = 'http://localhost:3000/commit';
+            this.$http.post(url, this.post, {emulateJSON:true}).then(function(res){
+                console.log(res)
+            }, function(res){
+                console.log(res)
+            })
+        }
     }
 });
--- routes/commit.js
+++ routes/commit.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var URL = require('url');
+var base = require('../base.js')
+
+// 创建连接
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'forum'
+});
+connection.connect();
+
+// 构造sql语句
+var sql = 'INSERT INTO posts(userId,title,message,images,likeNumber,lastModified) VALUE(?,?,?,?,0,?)'
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(404);
+  res.send();
 });
+
+/* POST user listing */
+router.post('/', function(req, res, next) {
+  var data = req.body;
+  var timestamp = new Date();
+  var sqlParams = [global.userInfo.id, data.title, data.message, data.imageList, timestamp]
+  connection.query(sql, sqlParams, function(err, result){
+    if(err){
+      base.sendErr(res, 2, err);
+    } else{
+      res.send({
+        code: 0,
+        data: {
+          message: '发帖成功'
+        }
+      });
+    }
+  })
+})
 
 module.exports = router;
